5cf29d4eced17180c4fc98957974ea4001317c86
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-25 13:41:33 -0300

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <h1 className="bg-zinc-500">Hello World</h1>
+    <div className="flex items-center justify-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
