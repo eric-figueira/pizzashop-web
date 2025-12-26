1b5acf1f12e797fcf6356b97709dfb6cccb2cb74
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2025-12-26 12:39:45 -0300

    feat: create account dropdown menu

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,33 @@
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex items-center gap-2 select-none">
+          Pizza Shop
+
+          <ChevronDown className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Eric Figueira</span>
+          <span className="text-xs font-normal text-muted-foreground">eric@pizzashop.com.br</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="size-4 mr-1" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="size-4 mr-1 text-rose-500 dark:text-rose-400" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
