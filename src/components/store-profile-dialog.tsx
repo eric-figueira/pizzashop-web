2a1696daa19bb92374ca309b9d74fd86942ce50b
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-06 08:23:48 -0300

    refactor: add optimistc interface logic

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -14,7 +14,7 @@ import { Spinner } from "./ui/spinner";
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
-  description: z.string(),
+  description: z.string().nullable(),
 })
 
 type StoreProfile = z.infer<typeof storeProfileSchema>
@@ -41,17 +41,32 @@ export function StoreProfileDialog() {
     },
   })
 
+  function updateManagedRestaurantCache({ name,description }: StoreProfile) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+      
+    if (cached) {
+      queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
+        ...cached,
+        name,
+        description,
+      })
+    }
+
+    return { cached }
+  }
+
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onSuccess(_, { name, description }) {
-      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
-      
-      if (cached) {
-        queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
-          ...cached,
-          name,
-          description,
-        })
+    onMutate({ name, description }) {
+      const { cached } = updateManagedRestaurantCache({ name, description })
+
+      return { previousProfile: cached }
+
+    },
+    // optimistc interface
+    onError(_error, _variables, context) {
+      if (context?.previousProfile) {
+        updateManagedRestaurantCache(context.previousProfile)
       }
     }
   })
