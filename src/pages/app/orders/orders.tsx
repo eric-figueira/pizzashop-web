9598ac0e11d0cb70e480e89e8f47907c00025436
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-08 09:16:28 -0300

    feat: implement filters on orders table

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -11,6 +11,10 @@ import z from "zod";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
   
   const pageIndex = z.coerce
     .number()
@@ -18,8 +22,14 @@ export function Orders() {
     .parse(searchParams.get('page') ?? '1')
 
   const { data: result } = useQuery({
-    queryFn: () => getOrders({ pageIndex }),
-    queryKey: ['orders', pageIndex]
+    queryFn: () => 
+      getOrders({ 
+        pageIndex, 
+        orderId, 
+        customerName, 
+        status: status === 'all' ? null : status 
+      }),
+    queryKey: ['orders', pageIndex, orderId, customerName, status]
   })
 
   function handlePaginate(pageIndex: number) {
