fbf9b44dd32214d582730288c195ef17b5ef6bdf
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-17 08:39:01 -0300

    test: create sign-up e2e tests

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Nome do estabelecimento' }).fill('Pizza Shop')
+  await page.getByRole('textbox', { name: 'Seu nome' }).fill('John Doe')
+  await page.getByRole('textbox', { name: 'Seu e-mail' }).fill('johndoe@gmail.com')
+  await page.getByRole('textbox', { name: 'Seu celular' }).fill('919283193')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso.')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign up with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Nome do estabelecimento' }).fill('Invalid Name')
+  await page.getByRole('textbox', { name: 'Seu nome' }).fill('John Doe')
+  await page.getByRole('textbox', { name: 'Seu e-mail' }).fill('johndoe@gmail.com')
+  await page.getByRole('textbox', { name: 'Seu celular' }).fill('919283193')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
