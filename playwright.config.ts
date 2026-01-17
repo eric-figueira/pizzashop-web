37d502a25267e845ea49d31e399dd01e0f65b4be
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-17 09:24:30 -0300

    chore: rename tests folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './test',
+  testDir: './tests',
   testMatch: /.*\.e2e-spec\.ts$/,
   /* Run tests in files in parallel */
   fullyParallel: true,
