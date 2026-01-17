b110975a45a3dd6b804ffde36d12f091d11db7bb
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-17 08:30:40 -0300

    test: create sign-in e2e tests

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Seu e-mail' }).fill('admin@admin.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Enviamos um link de autenticação para o seu e-mail.')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('textbox', { name: 'Seu e-mail' }).fill('johndoe@admin.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to register restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
